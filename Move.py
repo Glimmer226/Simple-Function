675af56584a1ed4f3b247463973a9aa7b88b0de3
Glimmer226 <41518037@qq.com>
2018-01-11 17:39:33 +0800

    log function
    try to add console and file log to script

--- Move.py
+++ Move.py
@@ -4,6 +4,27 @@
 import os
 import re
 import shutil
+import logging
+
+
+'''Defined log configuration and output'''
+logger=logging.getLogger(__name__)
+Filename='Move.log'
+Filemode='a'
+Format='%(asctime)s %(filename)s %(process)d [line:%(lineno)d] %(levelname)s %(message)s'
+Datefmt='%a-%d-%b-%Y %H:%M:%S'
+#logging.basicConfig(filename=Filename,filemode=Filemode,format=Format,level=logging.DEBUG,datefmt=Datefmt)
+
+console=logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(Format)
+logger.addHandler(console)
+
+logfile=logging.FileHandler(Filename,mode=Filemode)
+logfile.setLevel(logging.DEBUG)
+logfile.setFormatter(Format)
+logger.addHandler(logfile)
+
 
 class Move:
     def __index__(self):
@@ -15,12 +36,15 @@ class Move:
         listdir = os.listdir(path)
         str = '\n'.join(listdir)
         #print path
+        logging.debug(path)
        # print listdir,'\n'
+        logging.debug(listdir)
        # print str,'\n'
+        logging.debug(str)
 
         pattern = re.compile(r'^\w*.py$',re.M)
         self.reslut = re.findall(pattern,str)
-        print 'self.result:',self.reslut
+        logging.info('self.result:%s'%self.reslut)
 
 
     def Movefile(self):
@@ -30,13 +54,17 @@ class Move:
         if os.path.isdir(dst):
             for files in self.reslut:
                 file = os.getcwd()+'\\'+files
-                print 'files have:',file
+                #print 'files have:',file
+                logging.info('files have: %s' % file)
                 shutil.copy(file,dst)
         else:
-            print "It's not a path!"
+            #print "It's not a path!"
+            logging.error("It's not a path!")
 
 
 a = Move()
 if __name__ == '__main__':
+    logger.addHandler(console)
+    logger.addHandler(logfile)
     a.Findfile()
     a.Movefile()
