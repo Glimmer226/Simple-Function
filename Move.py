83c6214b6203dfa74b584387ef3fa5a566937b77
Glimmer226 <41518037@qq.com>
2018-01-12 14:44:56 +0800

    add log
    add console log and file log

--- Move.py
+++ Move.py
@@ -1,5 +1,11 @@
 #encoding:utf-8
-'''Move specified postfix files to specified directory'''
+'''Move specified postfix files to specified directory
+   Author:Glimmer
+   V1.0: Move specified postfix files to specified directory
+   v1.1: Add log function to track error message
+   v1.2: Print log to console and log file.
+   '''
+
 
 import os
 import re
@@ -7,24 +13,22 @@ import shutil
 import logging
 
 
-'''Defined log configuration and output'''
+'''Defined logger'''
 logger=logging.getLogger(__name__)
-Filename='Move.log'
-Filemode='a'
-Format='%(asctime)s %(filename)s %(process)d [line:%(lineno)d] %(levelname)s %(message)s'
-Datefmt='%a-%d-%b-%Y %H:%M:%S'
-#logging.basicConfig(filename=Filename,filemode=Filemode,format=Format,level=logging.DEBUG,datefmt=Datefmt)
+logger.setLevel(logging.DEBUG)
 
+'''Defined console log'''
 console=logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-console.setFormatter(Format)
+formatter=logging.Formatter(r'%(asctime)s-%(filename)s[line:%(lineno)d]-%(levelname)s: %(message)s')
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
 logger.addHandler(console)
 
-logfile=logging.FileHandler(Filename,mode=Filemode)
-logfile.setLevel(logging.DEBUG)
-logfile.setFormatter(Format)
-logger.addHandler(logfile)
-
+'''Defined files log'''
+files=logging.FileHandler(filename='Move.log',mode='a')
+files.setFormatter(logging.DEBUG)
+files.setFormatter(formatter)
+logger.addHandler(files)
 
 class Move:
     def __index__(self):
@@ -36,35 +40,34 @@ class Move:
         listdir = os.listdir(path)
         str = '\n'.join(listdir)
         #print path
-        logging.debug(path)
+        logger.debug(path)
        # print listdir,'\n'
-        logging.debug(listdir)
+        logger.debug(listdir)
        # print str,'\n'
-        logging.debug(str)
+        logger.debug(str)
 
         pattern = re.compile(r'^\w*.py$',re.M)
         self.reslut = re.findall(pattern,str)
-        logging.info('self.result:%s'%self.reslut)
+        logger.info('self.result:%s'%self.reslut)
 
 
     def Movefile(self):
         '''Move the files to specified directory'''
         dst = raw_input(r"Enter the destination directory path: ")
+        logger.info('Inpurt destination path: %s' % dst)
         #dst = 'C:\\Users\\xzhang\\Desktop\\Script\\python script'
         if os.path.isdir(dst):
             for files in self.reslut:
                 file = os.getcwd()+'\\'+files
                 #print 'files have:',file
-                logging.info('files have: %s' % file)
+                logger.info('files have: %s' % file)
                 shutil.copy(file,dst)
         else:
             #print "It's not a path!"
-            logging.error("It's not a path!")
+            logger.error("It's not a path!")
 
 
 a = Move()
 if __name__ == '__main__':
-    logger.addHandler(console)
-    logger.addHandler(logfile)
     a.Findfile()
     a.Movefile()
