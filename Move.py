577d1ef8a333740c8ff2e6eac93445968927765f
Glimmer226 <41518037@qq.com>
2018-01-18 15:36:01 +0800

    improve log

--- Move.py
+++ Move.py
@@ -5,6 +5,7 @@
    v1.1: Add log function to track error message
    v1.2: Print log to console and log file.
    v1.3: You can input specified postfix. Add usage,version.
+   V1.4: Improve log function.
    '''
 
 
@@ -14,91 +15,117 @@ import shutil
 import logging
 import getopt,sys
 
+class log():
+    def __init__(self,logger_name,log_name,csl=logging.INFO):
+        #time_str = time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time()))
+        self.logname = log_name + '.log'
+        self.log_file_dir = 'Move_log'
+        self.csl = csl
+        '''Defined logger'''
+        self.logger=logging.getLogger(logger_name)
+        self.logger.setLevel(logging.DEBUG)
+        self.formatter = logging.Formatter(r'%(asctime)s-%(filename)s[line:%(lineno)d]-%(levelname)s: %(message)s')
 
-'''Defined logger'''
-logger=logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+    def console(self):
+        '''Defined console log'''
+        console = logging.StreamHandler()
+        console.setLevel(self.csl)
+        console.setFormatter(self.formatter)
+        self.logger.addHandler(console)
 
-'''Defined console log'''
-console=logging.StreamHandler()
-formatter=logging.Formatter(r'%(asctime)s-%(filename)s[line:%(lineno)d]-%(levelname)s: %(message)s')
-console.setLevel(logging.INFO)
-console.setFormatter(formatter)
-logger.addHandler(console)
+    def file(self):
+        '''Defined files log'''
+        files = logging.FileHandler(filename=self.logname, mode='a')
+        files.setFormatter(logging.DEBUG)
+        files.setFormatter(self.formatter)
+        self.logger.addHandler(files)
 
-'''Defined files log'''
-files=logging.FileHandler(filename='Move.log',mode='a')
-files.setFormatter(logging.DEBUG)
-files.setFormatter(formatter)
-logger.addHandler(files)
+    def file_log(self):
+        if not os.path.isdir(self.log_file_dir):
+            self.logger.debug("Log file directory %s no exsit,will create it." % self.log_file_dir)
+            #print "Log file directory %s no exsit,will create it." % log_file_dir
+            os.mkdir(self.log_file_dir)
+            os.chdir(self.log_file_dir)
+            self.file()
+        else:
+            self.logger.debug("%s exsit" % self.log_file_dir)
+            #print "%s exsit" % log_file_dir
+            os.chdir(self.log_file_dir)
+            self.file()
+
+log = log('Move','Move',logging.INFO)
+log.console()
+log.file_log()
+
+def parseArgs():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'p:hvd', ["postfix=", "help", "version", "debug"])
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                usage()
+                sys.exit()
+            if o in ('-v', '--version'):
+                version()
+                sys.exit()
+            if o in ('-p', '--postfix'):
+                postfix = a
+                return postfix
+    except getopt.GetoptError:
+        usage()
+        sys.exit()
 
 class Move:
-    def __index__(self):
+    def __init__(self):
         pass
 
     def Findfile(self,postfix):
         '''Find the specified postfix files'''
+        os.chdir("C:\Users\zhangg14\Desktop\Python")
         path = os.getcwd()
         listdir = os.listdir(path)
         str = '\n'.join(listdir)
-        #print path
-        logger.debug(path)
-       # print listdir,'\n'
-        logger.debug(listdir)
-       # print str,'\n'
-        logger.debug(str)
+        log.logger.debug('Working direcroty is %s'% path)
+        log.logger.debug('Target files have %s'% listdir)
+        log.logger.debug('Separate files in list: %s'% str)
 
         Pattern = r'^\w*.%s$' % postfix #postfix: py,log,txt...
         pattern = re.compile(Pattern,re.M)
         self.reslut = re.findall(pattern,str)
-        logger.debug('self.result:%s'%self.reslut)
-
+        if self.reslut == None:
+            print "Have not find postfix files in this directory!"
+            #sys.exit()
+        log.logger.debug('self.result:%s'%self.reslut)
+        #sys.exit()
 
     def Movefile(self):
         '''Move the files to specified directory'''
         dst = raw_input(r"Enter the destination directory path: ")
-        logger.info('Inpurt destination path: %s' % dst)
+        log.logger.info('Input destination path: %s' % dst)
         #dst = 'C:\\Users\\xzhang\\Desktop\\Script\\python script'
         if os.path.isdir(dst):
             for files in self.reslut:
                 file = os.getcwd()+'\\'+files
                 #print 'files have:',file
-                logger.info('files have: %s' % file)
+                log.logger.info('files have: %s' % file)
                 shutil.copy(file,dst)
         else:
             #print "It's not a path!"
-            logger.error("It's not a path!")
+            log.logger.error("It's not a path!")
 
 def usage():
     print "usage: %s [-p|--postfix]{py|log|txt|} [-h|--help] [-v|--version]" % sys.argv[0]
 
 def version():
-    print '''    v1.0: Move specified postfix files to specified directory
-    v1.1: Add log function to track error message
+    print '''    v1.0: Move specified postfix files to specified directory.
+    v1.1: Add log function to track error message.
     v1.2: Print log to console and log file.
-    v1.3: You can input specified postfix'''
-
-def parseArgs():
-    try:
-        opts,args = getopt.getopt(sys.argv[1:],'p:hv',["postfix=","help","version"])
-        for o,a in opts:
-            if o in ('-h','--help'):
-                usage()
-                sys.exit()
-            if o in ('-v','--version'):
-                version()
-                sys.exit()
-            if o in ('-p','--postfix'):
-                postfix = a
-    except getopt.GetoptError:
-        usage()
-        sys.exit()
-    return postfix
+    v1.3: You can input specified postfix.
+    V1.4: Improve log function.'''
 
 def main():
+    p = parseArgs()
     a=Move()
-    postfix = parseArgs()
-    a.Findfile(postfix)
+    a.Findfile(p)
     a.Movefile()
 
 if __name__ == '__main__':
